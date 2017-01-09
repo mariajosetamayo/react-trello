f35303ef99e85417d40a3a0f85ebf8d9a04cfa4f
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2017-01-09 10:21:38 -0700

    changes to format

--- test/board-test.js
+++ test/board-test.js
@@ -14,8 +14,6 @@ describe ('Board component', function(){
   const renderer = TestUtils.createRenderer();
   renderer.render(<Board title = 'Trello board test' lists = {lists}/>);
   const result = renderer.getRenderOutput();
-  console.log('this is the result', result.props.children[1][0].props.children)
-  console.log('this is the result', result.props.children[1][0])
   result.type.should.equal('div');
   result.props.className.should.equal('card-list-board');
   result.props.children[1].should.have.length(3);
@@ -25,7 +23,6 @@ describe ('Board component', function(){
   board[0].props.children.should.equal('Trello board test');
 
   const listContainer = result.props.children[1][0]
-  console.log('this is the title',listContainer.props.children.props.listTitle.cards)
   listContainer.props.children.props.listTitle.title.should.equal('this is the title for the card test');
   listContainer.props.children.props.listTitle.cards.should.have.length(3);
   listContainer.props.children.props.listTitle.cards[0].text.should.equal('card1');
